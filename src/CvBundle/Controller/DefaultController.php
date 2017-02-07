3ce9fffa65e933e108c461cb7bb7c11b5818d89b
QuentinRillet <quentin.rillet@gmail.com>
2017-02-07 16:00:46 +0100

    Fix Security, Add Front-end integration

--- src/CvBundle/Controller/DefaultController.php
+++ src/CvBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CvBundle:Default:index.html.twig');
+        return $this->render('CvBundle:Front:index.html.twig');
     }
 }
